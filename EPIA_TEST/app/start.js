05f9d53bf2e52a68358453245cdcdb30536445b7
taewook1235 <wjdxodnr1234@naver.com>
2022-04-19 14:04:21 +0900

    test1

--- EPIA_TEST/app/start.js
+++ EPIA_TEST/app/start.js
@@ -352,7 +352,7 @@ function test_message(input_device){
         document.getElementById("TEST5").className = "main_grid_top_2"; // 사용 횟수
         document.getElementById("TEST6").className = "main_grid_top_2";
 
-        document.getElementById("toggle_id").hidden();
+        document.getElementById("toggle_id").hide();
         viewTest("버튼 검사", 
         "버튼을 아래 순서대로 누르고 결과(정상 동작시 괄호안에 O표시 됨)를 확인한다. <br><br>" +
         " 전진 버튼 : (" + input[5].substr(3,1) + ") <br>" + 
