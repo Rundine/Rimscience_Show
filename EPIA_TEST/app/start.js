cdb01633d2f90afb40021bd3291f6d17f8642f4c
taewook1235 <wjdxodnr1234@naver.com>
2022-05-03 10:40:41 +0900

    Test1

--- EPIA_TEST/app/start.js
+++ EPIA_TEST/app/start.js
@@ -433,7 +433,7 @@ function test_message(input_device){
                     " 75mm 이전에 멈추거나 이동거리가 증가하지않음 <br>" +
                     "&nbsp&nbsp > <font color='yellow'> 모터 뒷단에 홀 센서가 떨어져있는지 확인하여 떨어져있는 경우 가까이 붙이고 재시험한다. </font> <br>" +
                     "&nbsp&nbsp > <font color='yellow'> 재시험시 동일한 증상의 경우 모터를 교체한다. </font> <br>" + 
-                    " 이동시간이 28초 이상 <br>" + 
+                    " 이동시간이 28초 이상 <br>" +
                     "&nbsp&nbsp > <font color='yellow'> 모터에 부하가 생기는 요소를 확인한다 (전선이 걸려 부하가 생김) </font> <br>" + 
                     "&nbsp&nbsp > <font color='yellow'> 부하 요소가 없을 경우 모터를 교체한다. </font> <br>" );
 
